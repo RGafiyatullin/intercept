e5992ac236389aca0527e562e6f68e1406609242
Roman Gafiyatullin <r.gafiyatullin@me.com>
2021-09-01 23:50:58 +0300

    remove undesired eprintln

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,8 @@ mod filter;
 #[no_mangle]
 fn ctor() {
     let config = if let Some(config) = config::load() {
-        eprintln!("args: {:?}", std::env::args().collect::<Vec<_>>());
-        eprintln!("Config found: {:#?}", config);
+        // eprintln!("args: {:?}", std::env::args().collect::<Vec<_>>());
+        // eprintln!("Config found: {:#?}", config);
         config
     } else {
         return;
